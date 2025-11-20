376ee7900d664850e80949c5a710315f6def8568
ts19-in <ts19.in@gmail.com>
2025-11-21 11:31:19 +1300

    Implemented Roles: Tanveer

--- tanveer/login-page/src/App.js
+++ tanveer/login-page/src/App.js
@@ -1,37 +1,110 @@
-// src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ResetPassword from "./pages/ResetPassword";
-import Dashboard from "./pages/Dashboard";
-import Vehicles from "./pages/Vehicles"; // ✅ Added Vehicles page
+
+import UserDashboard from "./pages/Dashboard";
+//import CarOwnerDashboard from "./pages/CarOwnerDashboard";
+//import AdminDashboard from "./pages/AdminDashboard";
+
+import Vehicles from "./pages/Vehicles";
 import BookVehicle from "./pages/BookVehicle";
 import PaymentPage from "./pages/PaymentPage";
 import FeedbackPage from "./pages/FeedbackPage";
 import MyBookings from "./pages/MyBookings";
 import MyProfile from "./pages/MyProfile";
-function App() {
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
+export default function App() {
   return (
     <Router>
       <Routes>
-        {/* Default route = Login */}
+        {/* Public Routes */}
         <Route path="/" element={<Login />} />
-
-        {/* Other routes */}
         <Route path="/register" element={<Register />} />
         <Route path="/reset" element={<ResetPassword />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/vehicles" element={<Vehicles />} /> {/* ✅ Browse vehicles page */}
-        <Route path="/book/:vehicleName" element={<BookVehicle />} />
-        <Route path="/payment" element={<PaymentPage />} />
-<Route path="/feedback" element={<FeedbackPage />} />
-  <Route path="/my-bookings" element={<MyBookings />} />
-  <Route path="/my-profile" element={<MyProfile />} />
+
+        {/* USER */}
+        <Route
+          path="/Dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["User"]}>
+              <UserDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/vehicles"
+          element={
+            <ProtectedRoute allowedRoles={["User"]}>
+              <Vehicles />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/book/:vehicleName"
+          element={
+            <ProtectedRoute allowedRoles={["User"]}>
+              <BookVehicle />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/payment"
+          element={
+            <ProtectedRoute allowedRoles={["User"]}>
+              <PaymentPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/my-bookings"
+          element={
+            <ProtectedRoute allowedRoles={["User"]}>
+              <MyBookings />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* CAR OWNER */}
+        {/* <Route */}
+          {/* path="/carowner-dashboard" */}
+          {/* element={ */}
+            {/* <ProtectedRoute allowedRoles={["CarOwner"]}> */}
+              {/* <CarOwnerDashboard /> */}
+            {/* </ProtectedRoute> */}
+          {/* } */}
+        {/* /> */}
+
+        {/* ADMIN */}
+        {/* <Route */}
+          {/* path="/admin-dashboard" */}
+          {/* element={ */}
+            {/* <ProtectedRoute allowedRoles={["Admin"]}> */}
+              {/* <AdminDashboard /> */}
+            {/* </ProtectedRoute> */}
+          {/* } */}
+        {/* /> */}
+
+        {/* SHARED */}
+        {/* <Route */}
+          {/* path="/my-profile" */}
+          {/* element={ */}
+            {/* <ProtectedRoute allowedRoles={["User", "CarOwner", "Admin"]}> */}
+              {/* <MyProfile /> */}
+            {/* </ProtectedRoute> */}
+          }
+        {/* /> */}
+
+        <Route path="/unauthorized" element={<h2>Access Denied</h2>} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
